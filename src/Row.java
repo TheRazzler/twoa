a2ec2f24d269996febb58d8553bb91b2939d1c28
Spencer Yoder <smyoder@ncsu.edu>
2019-04-02 19:57:13 -0400

    Updated input/output files

---
+++ src/Row.java
@@ -0,0 +1,16 @@
+public class Row {
+  public Row(int numCols) {
+    
+  }
+  
+  private class Column {
+    private boolean max;
+    private boolean min;
+    private boolean numeric;
+    
+    private double value;
+    private String symbol;
+    
+    private Column()
+  }
+}
---
+++ src/RowFactory.java
@@ -0,0 +1,5 @@
+public class RowFactory {
+  public RowFactory(String header) {
+    
+  }
+}
